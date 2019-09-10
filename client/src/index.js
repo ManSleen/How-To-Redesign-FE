6810c59dab2c41de01ed1bbb07689d0f12d9b403
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-10 08:55:34 -0600

    Added logger middleware

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { reducer } from "./store/reducers";
+import { logger } from "redux-logger";
 
 import "./index.css";
 import App from "./App";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
