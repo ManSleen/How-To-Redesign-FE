bf1b5666027efe254a4fff353198ae6170b34e4c
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-12 08:49:58 -0600

    Added sub nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from './components/NavBar.js'
+import SubNavBar from './components/SubNavBar'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SubNavBar />
       <h1>Yo I'm App!</h1>
     </div>
   );
---
+++ client/src/components/SubNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubNavBar = () => {
+    return(
+        <div className="sub-nav-bar-container">
+            I'm the sub nav my dude
+        </div>
+    )
+}
+
+export default SubNavBar;
