4e425d4e824422d13815f06357cb82f683ae7fb6
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-30 08:51:18 -0600

    Added MUI form elements to add-guide form

--- client/src/components/GuideForm/GuideForm.js
+++ client/src/components/GuideForm/GuideForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 
 import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import { makeStyles } from '@material-ui/core/styles';
 import ActionButton from '../../assets/buttons/ActionButton';
 
@@ -15,16 +19,32 @@ const useStyles = makeStyles(theme => ({
   },
   menu: {
     width: 200
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  formControl: {
+    marginTop: theme.spacing(2),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
   }
 }));
 
 const GuideForm = ({ history }) => {
   const classes = useStyles();
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
 
   const [guide, setGuide] = useState({
-    name: '',
-    email: '',
-    username: '',
+    title: '',
+    description: '',
+    category: '',
     password: ''
   });
 
@@ -43,10 +63,52 @@ const GuideForm = ({ history }) => {
     <div className="guide-form-container">
       <h3>Add a Guide</h3>
       <form onSubmit={handleSubmit}>
-        <input />
-        <input />
-        <input />
-        <input />
+        <TextField
+          fullWidth
+          id="outlined-name"
+          label="Title"
+          name="title"
+          className={classes.textField}
+          value={guide.title}
+          onChange={handleChange}
+          margin="normal"
+          variant="outlined"
+          autoComplete="name"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          name="description"
+          multiline
+          fullWidth
+          rows="4"
+          onChange={handleChange}
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel ref={inputLabel} htmlFor="outlined-age-simple">
+            Category
+          </InputLabel>
+          <Select
+            value={guide.category}
+            onChange={handleChange}
+            labelWidth={labelWidth}
+            inputProps={{
+              name: 'category',
+              id: 'outlined-age-simple'
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={'tech'}>Tech</MenuItem>
+            <MenuItem value={'craft'}>Craft</MenuItem>
+            <MenuItem value={'outdoors'}>Outdoors</MenuItem>
+            <MenuItem value={'food'}>Food</MenuItem>
+          </Select>
+        </FormControl>
         <button>Submit</button>
       </form>
     </div>
