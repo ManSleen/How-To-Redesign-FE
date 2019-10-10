c8033c45148ff3865118d393d5893188434cf943
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-09 22:16:46 -0600

    Styling looking good on dropzone areas

--- client/src/components/GuideForm/AddStep.js
+++ client/src/components/GuideForm/AddStep.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { useDropzone } from 'react-dropzone';
 
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
@@ -34,6 +35,26 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const AddStep = ({ history, addStep, guideSteps, setGuideSteps }) => {
+  const maxImageSize = 5242880;
+
+  const {
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    isDragReject,
+    acceptedFiles,
+    rejectedFiles
+  } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFiles, rejectedFiles) => {
+      console.log('acceptedFiles', acceptedFiles);
+      console.log('rejectedFiles', rejectedFiles);
+    }
+  });
+
+  const isFileTooLarge =
+    rejectedFiles.length > 0 && rejectedFiles[0].size > maxImageSize;
+
   const classes = useStyles();
 
   const [step, setStep] = useState({
@@ -77,9 +98,32 @@ const AddStep = ({ history, addStep, guideSteps, setGuideSteps }) => {
         />
         <div className="guide-photo-upload-input">
           <h4>Upload Instruction Images</h4>
-          <div className="drop-zone-image-upload">
-            <CameraIcon />
-            <p>Choose Images to upload</p>
+
+          <div {...getRootProps({ className: 'dropzone' })}>
+            <input {...getInputProps()} />
+            <div
+              className={`drop-zone-image-upload${
+                isDragActive ? ' active' : ''
+              }${isDragReject ? ' rejected' : ''}`}
+            >
+              <CameraIcon />
+
+              {!isDragActive && <p>Click or drag a file to upload</p>}
+              {isDragActive && !isDragReject && <p>Drop it here my dude!</p>}
+
+              {isFileTooLarge && <div className="error">File is too large</div>}
+              {isDragReject && (
+                <div className="error">
+                  <p>Images only please</p>
+                </div>
+              )}
+              <ul>
+                {acceptedFiles.length > 0 &&
+                  acceptedFiles.map(acceptedFile => (
+                    <li>{acceptedFile.name}</li>
+                  ))}
+              </ul>
+            </div>
           </div>
         </div>
         <TextField
@@ -97,7 +141,7 @@ const AddStep = ({ history, addStep, guideSteps, setGuideSteps }) => {
         />
 
         <button type="submit" className="add-another-step">
-          <p>Add Step</p>
+          <p>Add Another Step</p>
           <svg
             width="24"
             height="24"
